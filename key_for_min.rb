0c92e39eba2e4c60ededad06c4ce5d60944c8f8e
Sarah Allrozamo <sarahallrozamo@gmail.com>
2020-09-19 22:00:41 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,8 +9,6 @@ def key_for_min_value(name_hash)
     if lowest_value == nil || lowest_value < num
       lowest_value = num
       lowest_key = word
-    else
-      return nil
     end
   end
   lowest_key
