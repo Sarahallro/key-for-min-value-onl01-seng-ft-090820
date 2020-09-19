36e417601b53c8690694a7f2abc648533c9ca610
Sarah Allrozamo <sarahallrozamo@gmail.com>
2020-09-19 22:04:02 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   lowest_key = nil
   lowest_value = 0
   name_hash.each do |word, num|
-    if lowest_value == nil || lowest_value > num
+    if lowest_value == nil || num < lowest_value
       lowest_value = num
       lowest_key = word
     end
